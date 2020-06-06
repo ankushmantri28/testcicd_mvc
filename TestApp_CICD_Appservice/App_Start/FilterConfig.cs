5d43ad0b2de0c884455e9e4d4dc333bc8a6ac840
ankushmantri28 <ankush28.am@gmail.com>
2020-06-06 18:16:32 +0530

    checkin mvc code for azure cicd appservice

---
+++ TestApp_CICD_Appservice/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace TestApp_CICD_Appservice
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ TestApp_CICD_Appservice/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestApp_CICD_Appservice.MvcApplication" Language="C#" %>
